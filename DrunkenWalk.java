6733fd1a9a00c23fc8bb6674682bb2291cb98f66
Nathaniel Havens <97206671+American-Patriot1@users.noreply.github.com>
2025-09-11 10:25:08 -0500

    almost done

--- DrunkenWalk.java
+++ DrunkenWalk.java
@@ -21,60 +21,6 @@ public class DrunkenWalk {
         y=startY;
     }
 
-    void takeStep(){
-        Random random1 = new Random();
-        int int1 = random1.nextInt(4);
-        int moveBy = 1;
-        currentSteps += moveBy;
-        switch (int1) {
-            case 0:
-                x+=moveBy;
-                break;
-            case 1:
-                x-=moveBy;
-                break;
-            case 2:
-                y+=moveBy;
-                break;
-            case 3:
-                y-=moveBy;
-                break;
-            default:
-                throw new AssertionError();
-        }
-    }
-
-    boolean moreSteps(){
-        if (currentSteps<=maxSteps){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    boolean inBounds(){
-        if ((x>=-1*squareRadius) && (x<=squareRadius) && ((y>=-1*squareRadius) && (y<=squareRadius))){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    void walk(){
-        if ((maxSteps-currentSteps)!=0){
-            int tempX=x;
-            int tempY=y;
-            takeStep();
-            if (moreSteps()==true){
-                if(inBounds()==false){
-                    fellOff+=1;
-                    x=tempX;
-                    y=tempY;
-                }
-                walk();
-            }
-        }
-    }
     void drunkWalk(){
         for(int i = 0; i < amtDrunks;i++){
             RandomWalk tempWalk = new RandomWalk(maxSteps,maxSteps);
@@ -82,23 +28,3 @@ public class DrunkenWalk {
             System.out.println(tempWalk);
         }
     }
-
-
-
-
-
-
-    //getters
-    public Integer getX(){
-        return this.x;
-    }
-    public Integer getY(){
-        return this.y;
-    }
-
-    //to string
-    public String toString(){
-        String word="X: "+x+"\nY: "+y+"\nMax Steps: "+maxSteps+"\nCurrent Steps: "+currentSteps+"\nSquare Size: "+squareRadius+"\nFell Off: "+fellOff;
-        return word;
-    }
-}
